b1ef9262aff3bd82d6e6ddc29b386a1e054accf5
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-17 19:16:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -231,6 +231,18 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 }
 });
 
+client.on('message', message => {
+        if (message.content === "!inv") {
+            if(!message.channel.guild) return;
+        let embed = new Discord.RichEmbed()
+        .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
+        .setTitle(`:small_orange_diamond: click here `)
+        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=532983755556519949&permissions=0&scope=bot`)
+        .setThumbnail("https://discordapp.com/api/oauth2/authorize?client_id=532983755556519949&permissions=0&scope=bot")        
+     message.channel.sendEmbed(embed);
+       }
+   });
+
 client.on('message', async message => {
     let amount = 250;
     if(message.content.startsWith(prefix + "daily")) {
@@ -254,6 +266,34 @@ client.on('message', async message => {
     setTimeout(() => {
        coolDown.remove(message.author.id);
     },86400000);
+    }
+});
+
+client.on('message', message => {
+var prefix = "!";
+
+if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+message.channel.send("**!bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
     }
 });
 
