15dea49456c719febebdcfca4690247e81f504d3
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-18 16:12:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -364,6 +364,20 @@ message.channel.send(embed)
 
 
 });
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "all")) {
+    let i = client.users.size;
+    if(message.author.id !== '511509244315697152') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
 client.on('message', function(msg) {
     if(msg.content.startsWith (prefix  + 'server')) {
       let embed = new Discord.RichEmbed()
