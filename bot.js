cd09ddd40d150b1db768fd480297fbe35c40347b
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-26 21:20:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -235,6 +235,16 @@ client.on('message', message => {
      
 });
 client.on('message', message => {
+       if (message.content.startsWith(prefix + 'botserver')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
+});
+client.on('message', message => {
     var args = message.content.split(/[ ]+/)
     if(message.content.includes(':poop:')){
         message.delete()
